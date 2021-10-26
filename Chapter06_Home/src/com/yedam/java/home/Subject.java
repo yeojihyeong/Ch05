3aa78aea8aedd7b6cf6b29bcfd71b9d194cf9abf
admin <admin@DESKTOP-G41G93U>
2021-10-26 12:09:45 +0900

    gg

---
+++ Chapter06_Home/src/com/yedam/java/home/Subject.java
@@ -0,0 +1,22 @@
+package com.yedam.java.home;
+
+public class Subject {
+	private String name;	//과목 이름
+	private int scorePoint;	//과목 점수
+	
+	//각 필드의 getter, setter 메소드 구현
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScorePoint() {
+		return scorePoint;
+	}
+	public void setScorePoint(int scorePoint) {
+		this.scorePoint = scorePoint;
+	}
+	
+	
+}
